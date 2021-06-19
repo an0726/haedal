567624b891412a9e11809df9c6e7ff7ef47d9f6a
an0726 <82091116+an0726@users.noreply.github.com>
2021-06-19 14:37:00 +0900

    3주차 과제
    전자공학부 2021113438 안영석 과제제출

---
+++ 3주차 과제.py
@@ -0,0 +1,27 @@
+class sys:
+    sys_count = 0
+
+    def __init__(self, name, birth, key):
+        self.name = name
+        self.birth = birth
+        self.key = key
+        sys.sys_count += 1
+
+    def info(self):
+        print(f'이름: {self.name}')
+        print(f'생일 : {self.birth}')
+        print(f'신장: {self.key}')
+        print()
+
+    def attack(self, skill_num):
+        print(self.skill[skill_num])
+
+
+sys1 = sys('구서영', 629, 158)
+sys2 = sys('안영석', 726, 175)
+
+sys1.info()
+sys2.info()
+
+sys1.attack(1)
+sys2.attack(0)
